915f328edbe13045db097c5170fe63746d54cfc8
Mo Rich88 <46029097+azririch88@users.noreply.github.com>
2021-04-25 10:48:51 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,4 +29,10 @@ class App extends Component {
   }
 }
 
+export default withAuthenticator(App, true);import React, { Component } from 'react';import logo from './logo.svg';
+import './App.css'; function App()
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';Amplify.configure(aws_exports); class App extends Component { render() { return ( <div className="App"> <header className="App-header"> <img src={logo} className="App-logo" alt="logo" /> <h1>Hello from V2</h1> </header> </div> ); }} 
+
 export default withAuthenticator(App, true);
